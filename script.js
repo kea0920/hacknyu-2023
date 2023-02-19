836524ca2ce67449898949cccd7247dae03d858e
An Qi Ke <kea@bxscience.edu>
2023-02-19 10:53:37 -0500

    updated quiz

--- script.js
+++ script.js
@@ -6,37 +6,35 @@ var bScore = 0
 var qCount = 0
 
 /* QUESTION SCHEMATICS */
-var buttonIDs = ["0"]
-var buttonElementByID = ["0"]
-var buttonEventListener = ["0"]
+// var buttonIDs = ["0"]
+// var buttonElementByID = ["0"]
+// var buttonEventListener = ["0"]
 
-buttonIDs = generateButtonIDs()
-buttonElementByID = getButtonElementByID()
-buttonEventListener = addButtonEventListeners()
+// buttonIDs = generateButtonIDs()
+// buttonElementByID = getButtonElementByID()
+// buttonEventListener = addButtonEventListener()
 
-var buttonNum = buttonIDs.length();
+// var buttonNum = buttonIDs.length();
 
-const hiddenElements =  document.querySelectorAll('.hidden');
+// var questions = [
+//   "0", // placeholder value
+//   "q1",
+//   "q2",
+//   "q3",
+//   "q4",
+//   "q5",
+//   "q6",
+//   "q7",
+//   "q8"
+// ]
 
-var questions = [
-  "0", // placeholder value
-  "q1",
-  "q2",
-  "q3",
-  "q4",
-  "q5",
-  "q6",
-  "q7",
-  "q8"
-]
-
-var questionNum = questions.length();
-var answerNum = 2;
+// var questionNum = questions.length();
+// var answerNum = 2;
 
 /* FACT SCHEMATICS */
-var factIDs = ["0"]
-factIDs = generateFactIDs();
-var factIDNum = factIDs.length();
+// var factIDs = ["0"]
+// factIDs = generateFactIDs();
+// var factIDNum = factIDs.length();
 
 /* FACTS */
 // q1-q2 = water
@@ -44,8 +42,7 @@ var factIDNum = factIDs.length();
 // q5-q6 = agriculture
 // q7-q8 = transportation
 
-var posFacts = [
-  "0", // placeholder (not used); easier to assign quotes to questions
+let posFacts = [
   "Good job! An average shower uses about 2.5 gallons of water per minute. Water scarcity is a huge environmental issue today where 1.1 billion people lack access to water and 2.7 billion experience water scarcity at least one month a year. By 2025, two-thirds of the world’s population may be facing water shortages. (source: worldwildlife.org)",
   "It’s good to use less oil because oil can lead to spills which have very terrible effects on the organisms living there and seabirds in that area. In addition, it will have a harmful effect on the fishing economy because many of the fish there will have died.",
   "Good! Try to unplug appliances when not using them. This is because of the phantom effect, which is where appliances that are plugged in but not being used still generate a very small amount of energy. However, this small amount of energy is being generated at all times and it builds up ",
@@ -56,8 +53,7 @@ var posFacts = [
   "This is good, and try to fly as little as possible. Planes emit a lot of CO2 which is bad for the environment because it leads to more climate change"
 ]
 
-var negFacts = [
-  "0", // placeholder (not used); easier to assign quotes to questions
+let negFacts = [
   "The average shower uses 2.5 gallons of water per minute. That means you are using at least 25 gallons of water for your shower. Do better.",
   "Usage of oil as energy can have many environmental problems, with one being the risk of oil spills. These oil spills are very hard to clean up and pollute the oceans. This leads to the death of many sea animals, plants and birds.",
   "Even when you are not using appliances, you are still using a very small amount of energy by having them plugged in. While the amount of energy this uses is incredibly small compared to the amount of energy they use when they are actually being used, this builds up considering all of the appliances people have plugged in all around the world. This is called the phantom effect. You can therefore reduce your energy consumption by unplugging appliances when you aren't using them.",
@@ -69,19 +65,45 @@ var negFacts = [
 ]
 
 
-var facts = ["0"]
-facts = displayFacts();
+// var facts = ["0"]
+// facts = displayFacts();
 
 var result = document.getElementById("result")
-addButtonEventListeners();
+var button = document.getElementById("restart");
+
 
 // COMMENT OUT FROM HERE ONCE CONFIRMED WORKING
 var q1a1 = document.getElementById("q1a1")
 var q1a2 = document.getElementById("q1a2")
 var q2a1 = document.getElementById("q2a1")
 var q2a2 = document.getElementById("q2a2")
+
 var q3a1 = document.getElementById("q3a1")
 var q3a2 = document.getElementById("q3a2")
+var q4a1 = document.getElementById("q4a1")
+var q4a2 = document.getElementById("q4a2")
+
+var q5a1 = document.getElementById("q5a1")
+var q5a2 = document.getElementById("q5a2")
+var q6a1 = document.getElementById("q6a1")
+var q6a2 = document.getElementById("q6a2")
+
+var q7a1 = document.getElementById("q7a1")
+var q7a2 = document.getElementById("q7a2")
+var q8a1 = document.getElementById("q8a1")
+var q8a2 = document.getElementById("q8a2")
+
+var water1 = document.getElementById("water1")
+var water2 = document.getElementById("water2")
+
+var home1 = document.getElementById("home1")
+var home2 = document.getElementById("home2")
+
+var agri1 = document.getElementById("agri1")
+var agri2 = document.getElementById("agri2")
+
+var transportation1 = document.getElementById("transportation1")
+var transportation2 = document.getElementById("transportation2")
 
 q1a1.addEventListener("click", waterG1);
 q1a2.addEventListener("click", waterB1);
@@ -103,63 +125,169 @@ q7a2.addEventListener("click", transportationB1);
 q8a1.addEventListener("click", transportationG2);
 q8a2.addEventListener("click", transportationB2);
 
-// displayed text
-function waterG1(){
-  water1.innerHTML = factsG[0];
+button.addEventListener("click", restart);
+// COMMENT OUT END HERE
+
+/* GENERATE QUESTIONS */
+// creates a new question div class
+// function generateQuestion(questionNum, answer1, answer2){
+//   var question = generateQuestionStarter(questionNum) + generateAnswers(answer1, answer2) + generateQuestionFinisher
+//   return question
+// }
+
+// // create question starter
+// function generateQuestionStarter(questionNum){
+//   return(
+//     '<div class="question">' +
+//     '<h3>' + questions[questionNum] + '</h3>'
+//   )
+// }
+
+// // create question finisher
+// function generateQuestionFinisher(){
+//   return(
+//     '</div>'
+//   )
+// }
+
+// // creates a new answer div class
+// function generateAnswers(answer1, answer2){
+//   return(
+//     '<div class="answer-choice">' +
+//       '<br>' +
+//       '<button id="' + buttonIDs[answer1] + '">'+ buttonNames[answer1] + '</button>' +
+//     '</div>' +
+//     '<div class="answer-choice">' +
+//       '<br>' +
+//       '<button id="' + buttonIDs[answer2] + '">'+ buttonNames[answer2] + '</button>' +
+//     '</div>'
+//   )
+// }
+
+// /* BUTTONS */
+// // create button ids
+// function generateButtonIDs(answerNum){
+//   var buttonName = "";
+//   // test out this logic!
+//   for (let i = 1; i < questionNum; i++){
+//     for (let j = 1; j < answerNum + 1; j++){
+//       buttonName = "q" + i + "a" + j;
+//       buttonIDs.append(buttonName);
+//     }
+//   }
+// }
+
+// // get button element by id
+// function getButtonElementByID(){
+//   var elementID = "";
+//   for (i = 1; i < buttonNum; i++){
+//     elementID = document.getElementById(buttonIDs[i]);
+//     buttonElementByID.append(elementID);
+//   }
+// }
+
+// // add button event listeners for button *elements*
+// function addButtonEventListeners(){
+//   for (i = 1; i < buttonNum; i++){
+//     buttonElementByID[i].addEventListener("click", facts[i]);
+//   }
+// }
+
+// // creates a fact id in the form: q#f#
+// function generateFactIDs(){
+//   var factID = "";
+//   for (i = 1; i < questionNum; i++){
+//     for (j = 1; j < answerNum + 1; j++){
+//       factID = "q" + i + "f" + j;
+//       factIDs.append(factID);
+//     }
+//   }
+// }
+
+// /* GENERATE FACTS */
+// function displayFacts(){
+//   var currentNegFact = 1
+//   var currentPosFact= 1
+
+//   for (j = 1; j < factIDNum; j++){
+//     if (j % 2 == 0){ // second fact; display neg fact
+//       var negFact = factIDs[j].innerHTML = displayNegFacts(currentNegFact);
+//       facts.append(negFact);
+//       currentNegFact++
+//     }
+//     else { // returns 1; first fact; display pos fact
+//       var posFact = factIDs[j].innerHTML = displayPosFacts(currentPosFact);
+//       facts.append(posFact)
+//       currentPosFact++
+//     }
+//   }
+// }
+
+// function displayPosFacts(currentQuestion){
+//   return posFacts[currentQuestion];
+// }
+
+// function displayNegFacts(currentQuestion){
+//   return negFacts[currentQuestion];
+// }
+
+// COMMENT OUT ONCE CONFIRMED WORKING
+function cWaterG1(){
+  water1.innerHTML = posFacts[0];
 }
-function waterB1(){
-  water1.innerHTML = factsB[0];
+function cWaterB1(){
+  water1.innerHTML = negFacts[0];
 }
-function waterG2(){
-  water2.innerHTML = factsG[1];
+function cWaterG2(){
+  water2.innerHTML = posFacts[1];
 }
-function waterB2(){
-  water2.innerHTML = factsB[1];
+function cWaterB2(){
+  water2.innerHTML = negFacts[1];
 }
 
-function homeG1(){
-  home1.innerHTML = factsG[2];
+function cHomeG1(){
+  home1.innerHTML = posFacts[2];
 }
-function homeB1(){
-  home1.innerHTML = factsB[2];
+function cHomeB1(){
+  home1.innerHTML = negFacts[2];
 }
-function homeG2(){
-  home2.innerHTML = factsG[3];
+function cHomeG2(){
+  home2.innerHTML = posFacts[3];
 }
-function homeB2(){
-  home2.innerHTML = factsB[3];
-}
-
-function agriG1(){
-  agri1.innerHTML = factsG[4];
+function cHomeB2(){
+  home2.innerHTML = negFacts[3];
 }
 
-function agriB1(){
-  agri1.innerHTML = factsB[4];
+function cAgriG1(){
+  agri1.innerHTML = posFacts[4];
 }
 
-function agriG2(){
-  agri2.innerHTML = factsG[5];
+function cAgriB1(){
+  agri1.innerHTML = negFacts[4];
 }
 
-function agriB2(){
-  agri2.innerHTML = factsB[5];
+function cAgriG2(){
+  agri2.innerHTML = posFacts[5];
 }
 
-function transportationG1(){
-  agri1.innerHTML = factsG[6];
+function cAgriB2(){
+  agri2.innerHTML = negFacts[5];
 }
 
-function transportationB1(){
-  agri1.innerHTML = factsB[6];
+function cTransportationG1(){
+  transportation1.innerHTML = posFacts[6];
 }
 
-function transportationG2(){
-  agri2.innerHTML = factsG[7];
+function cTransportationB1(){
+  transportation1.innerHTML = negFacts[6];
 }
 
-function transportationB2(){
-  agri2.innerHTML = factsB[7];
+function cTransportationG2(){
+  transportation2.innerHTML = posFacts[7];
+}
+
+function cTransportationB2(){
+  transportation2.innerHTML = negFacts[7];
 }
 
 //results
@@ -168,16 +296,19 @@ function waterG1(){
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
 
+  cWaterG1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
+
 function waterG2(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
 
+  cWaterG2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -187,37 +318,40 @@ function homeG1(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cHomeG1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
+
 function homeG2(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cHomeG2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
+
 function agriG1(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cAgriG1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
-function agrigG2(){
+
+function agriG2(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cAgriG2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -227,7 +361,7 @@ function transportationG1(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cTransportationG1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -237,7 +371,7 @@ function transportationG2(){
   gScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cTransportationG2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -248,7 +382,7 @@ function waterB1(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cWaterB1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -258,7 +392,7 @@ function waterB2(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cWaterB2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -268,7 +402,7 @@ function homeB1(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cHomeB1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -278,7 +412,7 @@ function homeB2(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cHomeB2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -288,37 +422,39 @@ function agriB1(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cAgriB1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
-function agrigB2(){
+function agriB2(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cAgriB2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
+
 function transportationB1(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cTransportationB1()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
   }
 }
+
 function transportationB2(){
   bScore += 1
   qCount += 1
   console.log("gScore = " + gScore + " and qCount =" + qCount)
-
+  cTransportationB2()
   if (qCount == 8){
     console.log("the quiz is done")
     getResult();
@@ -330,8 +466,33 @@ function getResult(){
     result.innerHTML = "You can do a lot more to help. Check out our tips page for more ways you can help"
     console.log("b")
   }
-  else{
-    result.innerHTML = "You’ve been doing great, keep doing what you’re doing! Check out the rest of our tip on how to conserve more energy!"
+  else if (bScore < gScore) {
+    result.innerHTML = "You’ve been doing great, keep doing what you’re doing! Check out the rest of our tips on how to conserve more energy!"
     console.log("g")
   }
+  else {
+    result.innerHTML = ""
+    console.log("no result")
+  }
+}
+
+function clearFacts(){
+  water1.innerHTML = "";
+  water2.innerHTML = "";
+  home1.innerHTML = "";
+  home2.innerHTML = "";
+  agri1.innerHTML = "";
+  agri2.innerHTML = "";
+  transportation1.innerHTML = "";
+  transportation2.innerHTML = "";
+}
+
+//restart function
+function restart() {
+  result.innerHTML = "Your result is... " + getResult();
+  qCount = 0;
+  gScore = 0;
+  bScore = 0;
+  console.log("questionCount = " + qCount + " goodScore = " + gScore + " badScore = " + bScore)
+  clearFacts()
 }
